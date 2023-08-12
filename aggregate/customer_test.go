ffe8e69eff6d584ac6a26e9a709cf6608149155d
Atsuhiro <atcheri@gmail.com>
2023-08-12 23:03:19 +0200

    test: add first aggregate tests
    using ensure package

---
+++ aggregate/customer_test.go
@@ -0,0 +1,51 @@
+package aggregate_test
+
+import (
+	"testing"
+
+	"github.com/atcheri/tavern-ddd-go/aggregate"
+	"github.com/iamkoch/ensure"
+)
+
+func TestCustomer_NewCustomer(t *testing.T) {
+	type testCase struct {
+		test string
+		name string
+		err  error
+	}
+
+	testCases := []testCase{
+		{
+			test: "Empty name validation",
+			name: "",
+			err:  aggregate.ErrInvalidPersonName,
+		},
+		{
+			test: "Valid name",
+			name: "Mister Withname",
+			err:  nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		var name string
+		var err error
+		t.Run(tc.test, func(t *testing.T) {
+			ensure.That("testing NewCustomer factory function", func(s *ensure.Scenario) {
+				s.Given(tc.test, func() {
+					name = tc.name
+				})
+
+				s.When("Calling the NewCustomer factory function", func() {
+					_, err = aggregate.NewCustomer(name)
+				})
+
+				s.Then("NewCustomer returns an error", func() {
+					if err != tc.err {
+						t.Errorf("expected = %v, but got = %v", err, tc.err)
+					}
+				})
+			}, t)
+		})
+	}
+}
