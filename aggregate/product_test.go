256793b720cb0c44c7339ccd4f11372bb7938fbb
Atsuhiro <atcheri@gmail.com>
2023-08-13 10:36:05 +0200

    feat: add product aggregate

---
+++ aggregate/product_test.go
@@ -0,0 +1,43 @@
+package aggregate_test
+
+import (
+	"testing"
+
+	"github.com/atcheri/tavern-ddd-go/aggregate"
+)
+
+func TestProduct_NewProduct(t *testing.T) {
+	type testCase struct {
+		test        string
+		name        string
+		description string
+		price       float64
+		expectedErr error
+	}
+
+	testCases := []testCase{
+		{
+			test:        "should return error if name is empty",
+			name:        "",
+			description: "",
+			price:       0,
+			expectedErr: aggregate.ErrMissingValues,
+		},
+		{
+			test:        "validvalues",
+			name:        "test",
+			description: "test",
+			price:       1.0,
+			expectedErr: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.test, func(t *testing.T) {
+			_, err := aggregate.NewProduct(tc.name, tc.description, tc.price)
+			if err != tc.expectedErr {
+				t.Errorf("Expected error: %v, got: %v", tc.expectedErr, err)
+			}
+		})
+	}
+}
