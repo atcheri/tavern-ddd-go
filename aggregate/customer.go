b7cdfc5ee59b81e92534aac3f1441de44dc0d9fb
Atsuhiro <atcheri@gmail.com>
2023-08-13 10:07:25 +0200

    feat: add customer methods

--- aggregate/customer.go
+++ aggregate/customer.go
@@ -36,3 +36,26 @@ func NewCustomer(name string) (Customer, error) {
 
 	return customer, nil
 }
+
+func (c *Customer) GetID() uuid.UUID {
+	return c.person.ID
+}
+
+func (c *Customer) SetID(id uuid.UUID) {
+	if c.person == nil {
+		c.person = &entity.Person{}
+	}
+
+	c.person.ID = id
+}
+
+func (c *Customer) SetName(name string) {
+	if c.person == nil {
+		c.person = &entity.Person{}
+	}
+	c.person.Name = name
+}
+
+func (c *Customer) GetName() string {
+	return c.person.Name
+}
