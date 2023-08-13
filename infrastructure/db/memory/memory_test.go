b54a369185ea8dcb9d4599548215a5eaad492dfc
Atsuhiro <atcheri@gmail.com>
2023-08-13 10:08:24 +0200

    test: add customer repo first unit tests

---
+++ infrastructure/db/memory/memory_test.go
@@ -0,0 +1,67 @@
+package memory
+
+import (
+	"testing"
+
+	"github.com/atcheri/tavern-ddd-go/aggregate"
+	"github.com/atcheri/tavern-ddd-go/domain/customer"
+	"github.com/google/uuid"
+	"github.com/iamkoch/ensure"
+)
+
+func TestMemory_GetCustomer(t *testing.T) {
+	type testCase struct {
+		name string
+		id   uuid.UUID
+		err  error
+	}
+
+	// Create a fake customer to add to repository
+	cust, err := aggregate.NewCustomer("Percy")
+	if err != nil {
+		t.Fatal(err)
+	}
+	id := cust.GetID()
+
+	// Create the repo to use, and add some test Data to it for testing
+	repo := MemoryRepository{
+		customers: map[uuid.UUID]aggregate.Customer{
+			id: cust,
+		},
+	}
+
+	testCases := []testCase{
+		{
+			name: "No Customer By ID",
+			id:   uuid.MustParse("f47ac10b-58cc-0372-8567-0e02b2c3d479"),
+			err:  customer.ErrCustomerNotFound,
+		}, {
+			name: "Customer By ID",
+			id:   id,
+			err:  nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		var err error
+		var id uuid.UUID
+		t.Run("Create a new Customer and get the ID", func(t *testing.T) {
+			ensure.That("testing the GetID method", func(s *ensure.Scenario) {
+				s.Given(tc.name, func() {
+					id = tc.id
+				})
+
+				s.When("Calling the GetID method", func() {
+					_, err = repo.Get(id)
+				})
+
+				s.Then("it returns the correspondin error", func() {
+					if err != tc.err {
+						t.Errorf("expected = %v, but got = %v", err, tc.err)
+					}
+				})
+			}, t)
+		})
+	}
+
+}
